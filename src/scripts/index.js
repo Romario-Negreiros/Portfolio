6504bec432bceaf169684bd7dbf91205b46f0b8a
Romario-Negreiros <nromario482@gmail.com>
2021-08-02 22:53:03 -0300

    fixed menu bug

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,15 +1,17 @@
 /*************** Mobile menu ****************/
 const menuMechanisms = () => {
-  const { body } = window.document;
-  body.style.overflowY === 'hidden' ? body.style.overflowY = 'visible' : body.style.overflowY = 'hidden';
-  const spans = [...document.querySelectorAll(".burguer")];
-  spans.forEach((span, index) => {
-    if (index === 1) span.classList.toggle("close-left");
-    else if (index === 2) span.classList.toggle("close-right");
-    else span.classList.toggle("fade");
-  });
-  const menuList = document.querySelector(".a-header__navList");
-  menuList.classList.toggle("a-header__mobile--active");
+  if (window.innerWidth <= 600) {
+    const { body } = window.document;
+    body.classList.toggle("body--active");
+    const spans = [...document.querySelectorAll(".burguer")];
+    spans.forEach((span, index) => {
+      if (index === 1) span.classList.toggle("close-left");
+      else if (index === 2) span.classList.toggle("close-right");
+      else span.classList.toggle("fade");
+    });
+    const menuList = document.querySelector(".a-header__navList");
+    menuList.classList.toggle("a-header__mobile--active");
+  }
 };
 
 const mobileMenu = document.querySelector(".mobile-menu__container");
@@ -18,11 +20,11 @@ mobileMenu.addEventListener("click", menuMechanisms);
 /*************** Animate on scroll ****************/
 const animations = () => {
   const { pageYOffset, innerHeight } = window;
-  const elementsToAnimate = [...document.querySelectorAll('[data-aos]')]
+  const elementsToAnimate = [...document.querySelectorAll("[data-aos]")];
   elementsToAnimate.forEach((element) => {
     if (
       element.offsetTop - pageYOffset <=
-      innerHeight - ((element.offsetTop - pageYOffset) / 2)
+      innerHeight - (element.offsetTop - pageYOffset) / 2
     ) {
       element.classList.add("animated");
     }
@@ -33,18 +35,18 @@ window.addEventListener("scroll", animations);
 window.addEventListener("load", animations);
 
 /*************** Smooth scroll on links ****************/
-const smoothScroll = event => {
+const smoothScroll = (event) => {
   event.preventDefault();
-  const element = document.querySelector(event.target.getAttribute('href'));
+  const element = document.querySelector(event.target.getAttribute("href"));
   window.scroll({
     top: element.offsetTop,
-    behavior: "smooth"
+    behavior: "smooth",
   });
 };
 
 const internLinks = [...document.querySelectorAll('[href^="#"]')];
-internLinks.forEach(link => {
-  link.addEventListener('click', smoothScroll);
+internLinks.forEach((link) => {
+  link.addEventListener("click", smoothScroll);
   // Faz com que o menu seja fechado ao clicar em um dos links internos.
-  link.addEventListener('click', menuMechanisms);
+  link.addEventListener("click", menuMechanisms);
 });
