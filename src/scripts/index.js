d662a34a4040a6e89d6458f3151ff9dbddd39689
Romario Negreiros <nromario482@gmail.com>
2021-08-05 14:24:42 -0300

    text to animation on skills + animation script

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,38 +1,40 @@
+import texts from "./texts.js";
+
 /*************** Mobile menu ****************/
 const menuMechanisms = () => {
-  if(window.innerWidth <= 600) {
-    window.document.body.classList.toggle('body--active') 
+  if (window.innerWidth <= 600) {
+    window.document.body.classList.toggle("body--active");
 
-    const spans = [...document.querySelectorAll('.burguer')]
+    const spans = [...document.querySelectorAll(".burguer")];
     spans.forEach((span, index) => {
-      if(index === 0) span.classList.toggle('close-left')
-      else if(index === 1) span.classList.toggle('close-right')
-      else span.classList.toggle('fade')
-    })
+      if (index === 0) span.classList.toggle("close-left");
+      else if (index === 1) span.classList.toggle("close-right");
+      else span.classList.toggle("fade");
+    });
 
-    const menu = document.querySelector('.a-header__navList')
-    menu.classList.toggle('a-header__mobile--active')
+    const menu = document.querySelector(".a-header__navList");
+    menu.classList.toggle("a-header__mobile--active");
   }
-}
+};
 
-const mobileMenuBtn = document.querySelector('.mobile-menu__container')
-mobileMenuBtn.addEventListener('click', menuMechanisms)
- 
+const mobileMenuBtn = document.querySelector(".mobile-menu__container");
+mobileMenuBtn.addEventListener("click", menuMechanisms);
+
 /*************** Smooth scroll on intern links ****************/
-const internLinks = [...document.querySelectorAll('[href^="#')]
-internLinks.forEach(link => {
-  link.addEventListener('click', event => {
-    event.preventDefault()
-    // Makes menu close 
-    menuMechanisms()
+const internLinks = [...document.querySelectorAll('[href^="#')];
+internLinks.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+    // Makes menu close
+    menuMechanisms();
     // Smooth scroll
-    const element = document.querySelector(event.target.getAttribute('href'))
+    const element = document.querySelector(event.target.getAttribute("href"));
     window.scroll({
       top: element.offsetTop,
-      behavior: "smooth"
-    })
-  })
-})
+      behavior: "smooth",
+    });
+  });
+});
 
 /*************** Animate on scroll ****************/
 const animations = () => {
@@ -50,3 +52,28 @@ const animations = () => {
 
 window.addEventListener("scroll", animations);
 window.addEventListener("load", animations);
+
+/*************** Animations on skills section  ****************/
+const insertContent = (event) => {
+  const tec = event.target.getAttribute('data-skill')
+  console.log(tec)
+  if (tec !== null) {
+    const obj = texts.find((obj) => obj.title === tec);
+    title.innerHTML = obj.title;
+    content.innerHTML = obj.description;
+  }
+};
+const skills = [...document.querySelectorAll(".skill")];
+const title = document.querySelector(".e-skills__contents h2");
+const content = document.querySelector(".e-skills__contents p");
+skills.forEach((skill) => {
+  skill.addEventListener("mouseover", insertContent);
+  skill.addEventListener("click", insertContent);
+  skill.addEventListener("mouseout", () => {
+    title.innerHTML = "Tecnologias";
+    content.innerHTML = `
+    Passe o mouse sobre os cards para ver um breve resumo da tecnologia. <br />
+    Se estiver em um dispositivo móvel, basta clicar nos cards para ver o resumo.
+    `;
+  });
+});
---
+++ src/scripts/texts.js
@@ -0,0 +1,53 @@
+const texts = [
+  {
+    title: "HTML",
+    description: `
+        Linguagem de marcação de hipertexto. <br /> 
+        Cria todo o corpo da página, textos, imagens etc. <br />
+        Experiência de 4 meses.
+      `,
+  },
+  {
+    title: "CSS",
+    description: `
+            Linguagem de estilização. <br />
+            Traz um visual melhor para o site, e também animações e efeitos. <br />
+            Experiência de 4 meses.
+        `,
+  },
+  {
+    title: "JavaScript",
+    description: `
+            Linguagem de programação. <br />
+            Entrega dinâmica ao site, tornando-o mais agradável para o usuário. <br />
+            Experiência de 4 meses.
+        `,
+  },
+  {
+    title: "React",
+    description: `
+            Biblioteca Javascript. <br />
+            Torna a construção e expansão do projeto mais fácil, e com uma melhor performance. <br />
+            Experiência de 2 meses.
+        `,
+  },
+  {
+    title: "TypeScript",
+    description: `
+            Linguagem de programação que se baseia no JavaScript. <br />
+            Adiciona tipagem estática ao JavaScript, trazendo benefícios como a captura de erros durante
+            o desenvolvimento. <br />
+            Experiência de 2 semanas.
+        `,
+  },
+  {
+    title: "SASS",
+    description: `
+            Pré-processador CSS. <br />
+            Auxília na criação e organização dos estilos. <br />
+            Experiência de 1 mes.
+        `,
+  },
+];
+
+export default texts;
