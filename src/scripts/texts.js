42dff7032c51db6b3f6814d9761cb6bc199df4ae
Romario Negreiros <nromario482@gmail.com>
2022-01-15 18:03:45 -0300

    updated texts + corrected wrapper's size in about me section

--- src/scripts/texts.js
+++ src/scripts/texts.js
@@ -4,7 +4,7 @@ const texts = [
     description: `
         Linguagem de marcação de hipertexto. <br /> 
         Cria todo o corpo da página, textos, imagens etc. <br />
-        Experiência de 9 meses.
+        Experiência de 10 meses.
       `,
   },
   {
@@ -12,7 +12,7 @@ const texts = [
     description: `
             Linguagem de estilização. <br />
             Traz um visual melhor para o site, e também animações e efeitos. <br />
-            Experiência de 9 meses.
+            Experiência de 10 meses.
         `,
   },
   {
@@ -20,7 +20,7 @@ const texts = [
     description: `
             Linguagem de programação. <br />
             Entrega dinâmica ao site, tornando-o mais agradável para o usuário. <br />
-            Experiência de 9 meses.
+            Experiência de 10 meses.
         `,
   },
   {
@@ -28,7 +28,7 @@ const texts = [
     description: `
             Sistema de versionamento de código. <br />
             Agiliza o trabalho em equipe, o versionamento do código, e de maneira bem simples. <br />
-            Experiência de 9 meses.
+            Experiência de 6 meses.
     `,
   },
   {
@@ -36,7 +36,7 @@ const texts = [
     description: `
             Biblioteca Javascript. <br />
             Torna a construção e expansão do projeto mais fácil, e com uma melhor performance. <br />
-            Experiência de 7 meses.
+            Experiência de 9 meses.
         `,
   },
   {
@@ -45,7 +45,7 @@ const texts = [
             Linguagem de programação que se baseia no JavaScript. <br />
             Adiciona tipagem estática ao JavaScript, trazendo benefícios como a captura de erros durante
             o desenvolvimento. <br />
-            Experiência de 5 meses.
+            Experiência de 6 meses.
         `,
   },
   {
@@ -53,7 +53,7 @@ const texts = [
     description: `
             Pré-processador CSS. <br />
             Auxília na criação e organização dos estilos. <br />
-            Experiência de 1 mes.
+            Experiência de 5 meses.
         `,
   },
   {
@@ -62,7 +62,7 @@ const texts = [
           Bilbioteca javascript. <br />
           Auxilia na manipulação do DOM, assim como traz outras soluções,
           como para fazer animações ou requisições HTTP. <br />
-          Experiência de 1 semana.
+          Experiência de 5 meses.
     `,
   },
 ];
