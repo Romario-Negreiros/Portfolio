db7a7ef43bc9d26bc9c9a462c720d6a61935f56d
Romario Negreiros <nromario482@gmail.com>
2022-04-08 12:39:09 -0300

    Added nodejs and postgresql to texts file

--- src/scripts/texts.js
+++ src/scripts/texts.js
@@ -1,70 +1,89 @@
 const texts = [
   {
-    title: "HTML",
+    title: 'HTML',
     description: `
         Linguagem de marcação de hipertexto. <br /> 
         Cria todo o corpo da página, textos, imagens etc. <br />
-        Experiência de 10 meses.
-      `,
+        Experiência de 1 ano.
+      `
   },
   {
-    title: "CSS",
+    title: 'CSS',
     description: `
             Linguagem de estilização. <br />
             Traz um visual melhor para o site, e também animações e efeitos. <br />
-            Experiência de 10 meses.
-        `,
+            Experiência de 1 ano.
+        `
   },
   {
-    title: "JavaScript",
+    title: 'JavaScript',
     description: `
             Linguagem de programação. <br />
             Entrega dinâmica ao site, tornando-o mais agradável para o usuário. <br />
-            Experiência de 10 meses.
-        `,
+            Experiência de 1 ano.
+        `
   },
   {
-    title: "Git",
+    title: 'Git',
     description: `
             Sistema de versionamento de código. <br />
             Agiliza o trabalho em equipe, o versionamento do código, e de maneira bem simples. <br />
-            Experiência de 6 meses.
-    `,
+            Experiência de 8 meses.
+    `
   },
   {
-    title: "React",
+    title: 'React',
     description: `
             Biblioteca Javascript. <br />
             Torna a construção e expansão do projeto mais fácil, e com uma melhor performance. <br />
-            Experiência de 9 meses.
-        `,
+            Experiência de 11 meses.
+        `
   },
   {
-    title: "TypeScript",
+    title: 'TypeScript',
     description: `
             Linguagem de programação que se baseia no JavaScript. <br />
             Adiciona tipagem estática ao JavaScript, trazendo benefícios como a captura de erros durante
             o desenvolvimento. <br />
-            Experiência de 6 meses.
-        `,
+            Experiência de 8 meses.
+        `
   },
   {
-    title: "SASS",
+    title: 'SASS',
     description: `
             Pré-processador CSS. <br />
             Auxília na criação e organização dos estilos. <br />
             Experiência de 5 meses.
-        `,
+        `
   },
   {
-    title: "Jquery",
+    title: 'Jquery',
     description: `
           Bilbioteca javascript. <br />
           Auxilia na manipulação do DOM, assim como traz outras soluções,
           como para fazer animações ou requisições HTTP. <br />
-          Experiência de 5 meses.
-    `,
+          Experiência de 2 meses.
+    `
   },
-];
+  {
+    title: 'NodeJs',
+    description: `
+          Ambiente para execução de codigo javascript fora do navegador. <br />
+          Possiblita a criação de aplicações com backends facilmente escalaveis,
+          utilizando javascript. <br />
+          Experiência de 3 meses.
+    `
+  },
+  {
+    title: 'PostgreSQL',
+    description: `
+          Banco de dados objeto-relacional. <br />
+          Armazena dados em tabelas, oferecenco funções como a integração de dados entre
+          tabelas utilizando chaves estrangeiras, além de possuir um alto desempenho com 
+          um reduzido custo de manutenção. <br />
+          Experiência de 3 meses.
+    `
+  }
+]
 
-export default texts;
+export default texts
