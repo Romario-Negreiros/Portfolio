04f498f9329d26dd505fe149d1a18a3b92e64ef2
Romario Negreiros <nromario482@gmail.com>
2022-06-04 18:12:35 -0300

    config: add webpack configs to development mode

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const HtmlWebpaclPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src', 'scripts', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'public'),
+    compress: true,
+    port: 9000
+  },
+  module: {
+    rules: [
+      { test: /\.css$/, use: [MiniCssExtractPlugin.loader, 'css-loader'] }
+    ]
+  },
+  plugins: [
+    new HtmlWebpaclPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html')
+    }),
+    new MiniCssExtractPlugin({ filename: 'index.css' })
+  ]
+}
