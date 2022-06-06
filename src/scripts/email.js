bf0fd6d7025f01c3ca0b2487f8df13f1150c0a65
Romario Negreiros <nromario482@gmail.com>
2022-06-06 16:43:04 -0300

    feat: create email sender class

---
+++ src/scripts/email.js
@@ -0,0 +1,17 @@
+import { PUBLIC_KEY, TEMPLATE_ID, SERVICE_ID } from './environment.js'
+import { send } from '@emailjs/browser'
+
+class Email {
+  static async send(templateParams) {
+    const hasSent = localStorage.getItem('hasSent')
+    if (!hasSent) {
+      send(SERVICE_ID, TEMPLATE_ID, templateParams, PUBLIC_KEY)
+    } else {
+      throw new Error(
+        'You have already sent a email, if you need to send another, here it is: nromario482@gmail.com'
+      )
+    }
+  }
+}
+
+export default Email
